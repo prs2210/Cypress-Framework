53bc4f6f65ccc642aa4199347a362d560db8ab38
prs2210 <testuser22seller@gmail.com>
2024-05-20 17:55:03 +0530

    Modified code

--- cypress/integration/examples/Test7.spec.js
+++ cypress/integration/examples/Test7.spec.js
@@ -5,7 +5,7 @@ describe('Handling Mouse Hover', function ()
     it('Mouse hover', function () 
     {
         cy.visit(Cypress.env('url')+"AutomationPractice/")
-        cy.get('.mouse-hover-content').invoke('show')
+        cy.get('.mouse-hover-content').scrollIntoView().invoke('show')
         cy.contains('Top').click({force:true})
         cy.url().should('include','top')
 
